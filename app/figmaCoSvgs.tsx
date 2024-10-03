b556a6c65c77cba5131970392dd13c438852de6f
RoyJake <royjake629@gmail.com>
2024-10-03 17:01:28 +0000

    creation the grid section of the homepage

---
+++ app/figmaCoSvgs.tsx
@@ -0,0 +1,12 @@
+import burst from "../public/svgs/burst.svg";
+import hourglass from "../public/svgs/hourglass.svg";
+import zag from "../public/svgs/zag.svg";
+import dots from "../public/svgs/dots.svg";
+import sun from "../public/svgs/sun.svg";
+import comet from "../public/svgs/comet.svg";
+import snake from "../public/svgs/snake.svg";
+import tri from "../public/svgs/tri.svg";
+
+const figma_collections = [burst, hourglass, zag, dots, sun, comet, snake, tri];
+
+export default figma_collections;
